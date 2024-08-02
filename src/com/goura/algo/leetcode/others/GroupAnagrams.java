94df33889bec9cdaee82e9f0092bd445360be878
Gourahari Das <gourahari.das@gmail.com>
2024-08-01 20:40:01 -0400

    WildCard Match solution added.

--- src/com/goura/algo/leetcode/others/GroupAnagrams.java
+++ src/com/goura/algo/leetcode/others/GroupAnagrams.java
@@ -1,7 +1,6 @@
 package com.goura.algo.leetcode.others;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class GroupAnagrams {
---
+++ src/com/goura/algo/leetcode/p0xx/LC44WildcardMatching.java
@@ -0,0 +1,34 @@
+package com.goura.algo.leetcode.p0xx;
+
+public class LC44WildcardMatching {
+    public boolean isMatch(String s, String p) {
+        int i = p.length();
+        int j = s.length();
+        boolean[][] dp = new boolean[i+1][j+1];
+        dp[0][0] = true;
+        for (int x = 1; x <= i; x++) {
+            dp[x][0] = p.charAt(x-1) == '*'? dp[x-1][0] : false;
+        }
+        for (int y = 1; y <= j; y++) {
+            dp[0][y] = false;
+        }
+        for (int x = 1; x <= i; x++) {
+            boolean asteriskFlag = dp[x-1][0];
+            for (int y = 1; y <= j; y++) {
+                if (p.charAt(x-1) == '*') {
+                    if (asteriskFlag) {
+                        dp[x][y] = true;
+                    } else {
+                        dp[x][y] = dp[x-1][y];
+                        asteriskFlag = dp[x][y];
+                    }
+                } else if (p.charAt(x-1) == s.charAt(y-1) || p.charAt(x-1) == '?') {
+                    dp[x][y] = dp[x-1][y-1];
+                } else {
+                    dp[x][y] = false;
+                }
+            }
+        }
+        return dp[p.length()][s.length()];
+    }
+}
